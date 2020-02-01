7395146d8fc3841eb39cfd6ea1d63fab94cf2927
Sam Johnson <sambjohnson94@gmail.com>
2020-01-31 23:18:33 -0800

    formatted flash2_merge & q2a_reformat for use in greater pipeline

--- flash2_merge.sh
+++ flash2_merge.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 # flash2_merge.sh
-# Desc: A simple script that runs flash2 to merge all fastqs in the current
+# DESCRIPTION: A simple script that runs flash2 to merge all fastqs in the current
 # 	directory.
 
-flashSeq(){
-	if [[ -x "flash2" || "$PATH/flash2" ]]; then 
-		echo "FLASh found, executing..."
-		for i in *_1.fastq; do			# Merge all _1 & _2 fastqs
-			REV=$(basename "$i" \
-				| sed 's/_1\.fastq/_2.fastq/')
-			echo "$i" "$REV"
-			flash2 "$i" "$REV" 2>&1	\
-				-o $(basename "$i" | sed 's/_1\.fastq/.flash/') \
-				| tee -a flash.log
-			done
-		for i in *_R1.fastq; do			# Merge the R1/R2 reads -___-
-			REV=$(basename "$i" \
-				| sed 's/_R1\.fastq/_R2.fastq/')
-			echo "$i" "$REV"
-			flash2 "$i" "$REV" 2>&1 \
-				-o $(basename "$i" | sed 's/_R1\.fastq/.flash/') \
-				| tee -a flash.log
-			done
-	fi
-}
+# Deps: flash2
+# NOTE: RUN AFTER: --- [fastq-dump script??]
+#	RUN BEFORE: -- q2a_reformat.sh
 
-#for i in *; do
-#	cd "$i"/original/fastqs/16S
-	flashSeq
-#	cd ../../../../
-#done
+shopt -s nullglob
+
+for i in *1.fastq; do			# Merge all _1 & _2 fastqs
+	if [ -f *_1.fastq ]; then
+		SUF_REGX="s/_1\.fastq/_2.fastq/"
+	elif [ -f *_R1.fastq ]; then
+		SUF_REGX="s/_R1\.fastq/_R2.fastq/"
+	elif [ -f *_R1.fastq ] && [ -f *_1.fastq ]; then
+		echo "Problems!"
+		exit 1
+	else
+		exit 0
+	fi	
+	REV=$(basename "$i" | sed $SUF_REGX) 
+	OUT_FILE=$(basename "$i" | sed -e 's/_\(\|R\)1\.fastq/.merged/')
+	echo "Merging: $i" "$REV"
+	./flash2 "$i" "$REV" 2>&1	\
+		-o $OUT_FILE \
+		| tee -a flash.log
+	done
+
+shopt -u nullglob
--- from_barbera/q2a_reformat.sh
+++ from_barbera/q2a_reformat.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# q2a_reformat.sh
+
+# DESCRIPTION: Reformats all fastq files in current directory to fasta
+
+# Deps: bbtools 'reformat' tool
+
+# NOTE: RUN AFTER: --- flash2_merge.sh
+#	RUN BEFORE: -- groupFormatter.sh
+
 for i in *.fastq; do
 	reformat.sh in=$i out=$(basename $i | sed 's/fastq/fasta/')
 done
