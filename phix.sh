008fc6c8a2a12d5b30eb38086ad10b7add2f449a
Sam Johnson <sambjohnson94@gmail.com>
2020-02-04 14:40:31 -0800

    prepped phix & screening scripts for order in overall script

--- phix.sh
+++ phix.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+# phix.sh
+
+# DESCRIPTION: Takes in a fasta file, phix databases in a directory named 
+# 	$PWD/phix_db/ and screens for PhiX contamination
+
+# Deps: bowtie2
+# Note: RUN AFTER: --- mothur > ~/code/shell/bioinf/screening_batch.txt
+#	RUN BEFORE: -- mothur > ~/code/shell/bioinf/phix_removal_batch.txt
+
 bowtie2 -x phix_db/PhiX_bowtie_db* -f -U samples.trim.contigs.fasta -S merged.bowtie --un merged.screened --al merged.PhiX --local -p 2
+
+echo "PhiX screened, generating PhiX accession number file..."
+grep ">" merged.PhiX > PhiX.accnos
