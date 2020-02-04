e22bfe552e74c5b86c12fa2fea741bc1e0a9d8e5
Sam Johnson <sambjohnson94@gmail.com>
2020-02-04 14:22:04 -0800

    phix.sh adjusted to local use, added phix_db

--- phix.sh
+++ phix.sh
@@ -1,1 +1,1 @@
-bowtie2 -x /share/amishalab-backedup/PoretPeterson/PhiX/PhiX_bowtie_db* -f -U samples.trim.contigs.fasta -S merged.bowtie --un merged.screened --al merged.PhiX --local -p 20
+bowtie2 -x phix_db/PhiX_bowtie_db* -f -U samples.trim.contigs.fasta -S merged.bowtie --un merged.screened --al merged.PhiX --local -p 2
